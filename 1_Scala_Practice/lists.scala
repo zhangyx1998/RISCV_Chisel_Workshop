24277eeababe92cadc4d666970fe92e6931dfe0b
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2021-07-16 17:43:13 +0800

    [Learn] Scala_Practice: lists

---
+++ 1_Scala_Practice/lists.scala
@@ -0,0 +1,12 @@
+val x = 7
+val	y = 14
+val list1 = List(1, 2, 3)
+val	list2 = x :: y :: y :: Nil
+val list3 = list1 ++ list2
+val m = list2.length
+val	s = list2.size
+val	headOfList = list1.head
+val	endOfList  = list1.tail
+val third      = list1(2)
+
+println(list3)
